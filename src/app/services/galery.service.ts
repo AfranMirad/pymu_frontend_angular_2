235de69badd75df3cfc5ee1704e3ebbfc617f38a
Afran Mirad <afranmirad@gmail.com>
2019-12-25 15:46:39 +0300

    about update commit

--- src/app/services/galery.service.ts
+++ src/app/services/galery.service.ts
@@ -13,21 +13,4 @@ export class GaleryService {
   getGaleries(): Observable<Galery[]> {
     return this.http.get<Galery[]>(this.path)
   }
-
-  /*
-  Bu kısım ikinci bir çözüm içindir
-
-
-  
-  path: string = "https://constructionworks.herokuapp.com/"
-
-  getGaleries(userid):Observable<Galery[]> {
-    if (**userid) {                                                                         Bu veri değişecek
-      return this.http.get<Galery[]>(this.path + "jobs?userId=" + **userid)
-    } else {
-      return this.http.get<Galery[]>(this.path + "jobs")
-    }
-
-  }
-  */
 }
